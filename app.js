b8f321bbec398bb440859d3aa5768adf21e739c6
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-22 10:03:51 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // require packages and used in the project
 const express = require('express')
+// require session
+const session = require('express-session')
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -16,6 +18,11 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
+app.use(session({
+  secret: 'ThisIsMySecret', // 這參數是session驗證id的字串，不會洩漏給用戶端
+  resave: false, // 若設定true,會在每一次使用者互動後，強制將 session 更新到 session store 裡
+  saveUninitialized: true // 強制將為初始化的 session 存回 session store。未初始化表示這個 session 是新的沒有被修改過，例如未登入的使用者的session
+}))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
