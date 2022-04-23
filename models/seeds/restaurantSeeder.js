39c32725178c90d3669015335ab8ea4a4f91af03
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-24 01:00:01 +0800

    modify user name required type

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -12,13 +12,11 @@ const restaurantList = require('../../restaurant.json').results
 
 const SEED_USERS = [
   {
-    name: 'user1',
     email: 'user1@example.com',
     password: '12345678',
     restaurants: restaurantList.slice(0, 3)
   },
   {
-    name: 'user2',
     email: 'user2@example.com',
     password: '12345678',
     restaurants: restaurantList.slice(3, 6)
@@ -31,7 +29,6 @@ db.once('open', () => {
       .genSalt(10)
       .then(salt => bcrypt.hash(seed_user.password, salt))
       .then(hash => User.create({
-        name: seed_user.name,
         email: seed_user.email,
         password: hash
       }))
